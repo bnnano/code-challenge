722ce1c6baa685a0d039386aea86e566310eb607
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-16 14:51:56 -0600

    Instantiating QueryClient within App component to make sure there's only one instance per client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  // Making sure QueryClient instance is created once per client
+  const [queryClient] = useState(() => new QueryClient());
+
   return <QueryClientProvider client={queryClient}>
     <Component {...pageProps} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 }
