aa1aa111d247034ed5915d75ed48a37b1530dfdf
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-16 16:24:32 -0600

    Adding API skeleton

---
+++ pages/api/pokemon-service.ts
@@ -0,0 +1,13 @@
+import { Pokemon } from "@/types/pokemon";
+
+export async function getAllPokemon(): Promise<Pokemon[]> {
+    return [];
+}
+
+export async function getPokemonById(id: string): Promise<Pokemon> {
+    return {};
+}
+
+export async function getPokemonByName(): Promise<Pokemon> {
+    return {}
+}
