c53f73302c48197064cc401cebff1b5c75c860dc
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-18 09:12:40 -0600

    Implementing methods to fetch Pokemon

--- pages/api/pokemon.ts
+++ pages/api/pokemon.ts
@@ -1,13 +1,22 @@
-import { Pokemon } from "@/types/pokemon";
+import { NamedResource } from '@/types/common';
+import { Pokemon } from '@/types/pokemon';
 
 export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 export const IMAGE_BASE_URL = 'https://img.pokemondb.net/artwork/large';
-export const WIKI_URL = 'https://bulbapedia.bulbagarden.net/wiki'
+export const WIKI_URL = 'https://bulbapedia.bulbagarden.net/wiki';
 
-export async function getAllPokemon(offset: number = 0, limit: number = 6): Promise<Pokemon[]> {
-    const response = await fetch(`${BASE_URL}?limit=${limit}&offset=${offset}`);
+export async function getAllPokemon(offset: number = 0, limit: number = 6) {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    );
     const data = await response.json();
-    return data;
+    const individualPokemonRequests =
+        data?.results?.map?.(async (pokemonNamedResource: NamedResource) => {
+            const response = await fetch(pokemonNamedResource.url);
+            const pokemon = await response.json();
+            return pokemon;
+        }) ?? [];
+    return Promise.all(individualPokemonRequests);
 }
 
 export async function getPokemonByName(name: string): Promise<Pokemon> {
