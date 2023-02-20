bf457cc99ddc80902438153e0cf42c3e36f0e5cb
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-20 11:33:01 -0600

    Making the Input a styled input

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -1,11 +1,20 @@
+import { styled } from '@stitches/react';
 import { PropsWithChildren } from 'react';
 
 export interface InputProps {
     placeholder: string;
 }
 
-const Input = (props: InputProps) => {
-    return <input {...props} />;
-};
+const Input = styled('input', {
+    height: '56px',
+    border: 'none',
+    variants: {
+        variant: {
+            searchBar: {
+                borderRadius: '8px 0px 0px 8px',
+            },
+        },
+    },
+});
 
 export default Input;
