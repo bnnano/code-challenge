1d18d4c949ea01dcc3144b053d0817c993f7ee0a
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-20 11:29:24 -0600

    Adding Google Fonts to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { QueryClientProvider, QueryClient, Hydrate } from 'react-query'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { QueryClientProvider, QueryClient, Hydrate } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Inter } from '@next/font/google';
 
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
+    // Making sure QueryClient instance is created once per client
+    const [queryClient] = useState(() => new QueryClient());
 
-  // Making sure QueryClient instance is created once per client
-  const [queryClient] = useState(() => new QueryClient());
-
-  return <QueryClientProvider client={queryClient}>
-    <Hydrate state={pageProps.dehydratedState}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </Hydrate>
-  </QueryClientProvider>
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <main className={inter.className}>
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools />
+                </main>
+            </Hydrate>
+        </QueryClientProvider>
+    );
 }
