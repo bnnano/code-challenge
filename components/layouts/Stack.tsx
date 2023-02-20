b7b58d0d9ed3b60e141a4782697016939b6f2445
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-20 11:33:52 -0600

    Minor Stack refactor

--- components/layouts/Stack.tsx
+++ components/layouts/Stack.tsx
@@ -1,12 +1,12 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
 
 export interface StackProps extends PropsWithChildren {
-    direction?: "row" | "column";
-    alignItems?: "start" | "end" | "center";
+    direction?: 'row' | 'column';
+    alignItems?: 'start' | 'end' | 'center';
 }
 
 const Stack = (props: StackProps) => {
-    const { children, direction = "row", alignItems = "start" } = props;
+    const { children, direction = 'row', alignItems = 'start' } = props;
     return <div>{children}</div>;
 };
 
