54b6e863e76a87a1983b3ab63fa3487906eab01c
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-20 11:32:46 -0600

    Making the button a styled button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,10 +1,22 @@
-import { PropsWithChildren } from "react";
+import { styled } from '@stitches/react';
+import { PropsWithChildren } from 'react';
 
 export interface ButtonProps extends PropsWithChildren {}
 
-const Button = (props: ButtonProps) => {
-    const { children } = props;
-    return <button>{children}</button>;
-};
+const Button = styled('button', {
+    backgroundColor: '$darkestBluegrey',
+    color: 'white',
+    borderRadius: '8px',
+
+    variants: {
+        variant: {
+            searchButton: {
+                borderRadius: '0px 8px 8px 0px',
+                height: '56px',
+                border: 'none',
+            },
+        },
+    },
+});
 
 export default Button;
