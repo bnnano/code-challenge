eab25050eef1d474ffa4ac45f4f19e32e71c8ab2
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 11:40:32 -0600

    Adding temporary specific styling to SearchBar

--- components/organisms/SearchableContent.tsx
+++ components/organisms/SearchableContent.tsx
@@ -1,15 +1,26 @@
-import { PropsWithChildren } from 'react';
+import { ChangeEventHandler, PropsWithChildren } from 'react';
 import SearchBar from '../molecules/SearchBar';
+import Stack from '../layouts/Stack';
+import { styled } from '@/stitches.config';
 
-export interface SearchableContentProps extends PropsWithChildren {}
+export interface SearchableContentProps extends PropsWithChildren {
+    placeholder: string;
+    buttonText: string;
+    onSubmit: (newString: string) => void;
+    onClear: () => void;
+}
+
+const StyledSearchBar = styled(SearchBar, {
+    flexBasis: '200px',
+});
 
 const SearchableContent = (props: SearchableContentProps) => {
-    const { children } = props;
+    const { children, ...searchBarProps } = props;
     return (
-        <div>
-            <SearchBar />
+        <Stack direction="column" gap="9">
+            <StyledSearchBar {...searchBarProps} />
             {children}
-        </div>
+        </Stack>
     );
 };
 
