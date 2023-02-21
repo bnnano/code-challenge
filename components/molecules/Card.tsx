5e0acd04264ca0f752d61a243ab9696ed6633085
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 11:37:41 -0600

    Added helper subcomponents specific to Card

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -2,6 +2,7 @@ import Image, { ImageProps } from 'next/image';
 import { ReactElement } from 'react';
 import Typography from '../atoms/Typography';
 import Stack from '../layouts/Stack';
+import { styled } from '@/stitches.config';
 
 export interface CardProps {
     id?: string;
@@ -11,21 +12,57 @@ export interface CardProps {
     actionSection?: ReactElement;
 }
 
+const CardContainer = styled(Stack, {
+    flexDirection: 'column',
+    width: '320px',
+});
+
+const ImageContainer = styled(Stack, {
+    position: 'relative',
+    width: '320px',
+    height: '167px',
+});
+
+const FitImage = styled(Image, {
+    objectFit: 'contain',
+    paddingTop: '$4',
+    paddingBottom: '$4',
+    paddingLeft: '$8',
+    paddingRight: '$8',
+});
+
+const CardBody = styled(Stack, {
+    padding: '$8',
+    backgroundColor: 'white',
+    borderRadius: '0 0 8px 8px',
+});
+
+const CardHeader = styled('header', {
+    backgroundColor: '$lightBrowngrey',
+    borderRadius: '8px 8px 0 0',
+});
+
 const Card = (props: CardProps) => {
     const { imageProps } = props;
     const { alt, ...restImageProps } = imageProps;
     const { title, description, actionSection } = props;
     return (
-        <Stack>
-            <header>
-                <Image alt={alt} {...restImageProps} />
-            </header>
-            <main>
-                <Typography variant="cardTitle">{title}</Typography>
-                <Typography>{description}</Typography>
-            </main>
-            <footer>{actionSection}</footer>
-        </Stack>
+        <CardContainer>
+            <CardHeader>
+                <ImageContainer>
+                    <FitImage alt={alt} {...restImageProps} />
+                </ImageContainer>
+            </CardHeader>
+            <CardBody direction="column" gap="8">
+                <main>
+                    <Stack direction="column" gap="3">
+                        <Typography variant="cardTitle">{title}</Typography>
+                        <Typography>{description}</Typography>
+                    </Stack>
+                </main>
+                <footer>{actionSection}</footer>
+            </CardBody>
+        </CardContainer>
     );
 };
 
