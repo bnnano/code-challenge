486e768ec1b72d2def638c312f15e2fc219b66cc
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 11:33:50 -0600

    Creating simple unparametrized grid

--- components/layouts/Grid.tsx
+++ components/layouts/Grid.tsx
@@ -1,10 +1,12 @@
-import { PropsWithChildren } from "react";
+import { styled } from '@/stitches.config';
+import { PropsWithChildren } from 'react';
 
 export interface GridProps extends PropsWithChildren {}
 
-const Grid = (props: GridProps) => {
-    const { children } = props;
-    return <div>{children}</div>;
-};
+const Grid = styled('div', {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, 320px)',
+    gap: '$8',
+});
 
 export default Grid;
