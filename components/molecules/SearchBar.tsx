5b66aef12eeedd01f5874ba32b3f7e82e6595b4d
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 11:38:15 -0600

    Adding internal state management for input text and event listeners to set global state onClear and onSubmit

--- components/molecules/SearchBar.tsx
+++ components/molecules/SearchBar.tsx
@@ -1,25 +1,40 @@
-import { styled } from '@stitches/react';
+import { styled } from '@/stitches.config';
 import Button from '../atoms/Button';
 import Input from '../atoms/Input';
 import Stack from '../layouts/Stack';
+import { ChangeEventHandler, useState } from 'react';
 
 export interface SearchBarProps {
     placeholder: string;
     buttonText: string;
+    onSubmit: (newString: string) => void;
+    onClear: () => void;
 }
 
 const SearchBar = (props: SearchBarProps) => {
-    const { placeholder, buttonText } = props;
+    const [inputText, setInputText] = useState('');
+    const { placeholder, buttonText, onSubmit, onClear } = props;
     return (
         <Stack>
-            <Input variant="searchBar" placeholder={placeholder} />
-            <Button variant="searchButton">{buttonText}</Button>
+            <Input
+                variant="searchBar"
+                placeholder={placeholder}
+                onBlur={(e) => setInputText(e.target.value)}
+                onChange={(e) => {
+                    if (!e.target.value) {
+                        onClear();
+                    }
+                }}
+            />
+            <Button variant="searchButton" onClick={() => onSubmit(inputText)}>
+                {buttonText}
+            </Button>
         </Stack>
     );
 };
 
 SearchBar.defaultProps = {
-    placeholder: 'Type a Pokémon name',
+    placeholder: 'Type something here',
     buttonText: 'Search',
 };
 
