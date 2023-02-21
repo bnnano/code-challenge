4db70fe561f53d53310b48177221586ed0a314d2
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-20 19:41:27 -0600

    Stitches config file installation

--- stitches.config.ts
+++ stitches.config.ts
@@ -2,7 +2,13 @@ import { createStitches } from '@stitches/react';
 
 export const { styled, css, getCssText } = createStitches({
     theme: {
+        media: {
+            bp1: '(min-width: 640px)',
+            bp2: '(min-width: 768px)',
+            bp3: '(min-width: 1024px)',
+        },
         colors: {
+            lightestBluegrey: '#a2c7dd',
             lightBluegrey: '#408AB6',
             bluegrey: '#436070',
             darkBluegrey: '#143C52',
@@ -12,9 +18,15 @@ export const { styled, css, getCssText } = createStitches({
             darkGrey: '#8A9DA8',
         },
         space: {
-            1: '5px',
-            2: '10px',
-            3: '15px',
+            1: '4px',
+            2: '8px',
+            3: '12px',
+            4: '16px',
+            5: '20px',
+            6: '24px',
+            7: '28px',
+            8: '32px',
+            9: '36px',
         },
         fontSizes: {
             1: '12px',
