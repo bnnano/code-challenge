08aaf7970afea5e511489c41533af41e3623af25
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 15:03:09 -0600

    Adding more variants to Grid component

--- components/layouts/Grid.tsx
+++ components/layouts/Grid.tsx
@@ -5,8 +5,54 @@ export interface GridProps extends PropsWithChildren {}
 
 const Grid = styled('div', {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, 320px)',
-    gap: '$8',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 320px))',
+    variants: {
+        columns: {
+            1: {
+                gridTemplateColumns: 'repeat(1, 1fr)',
+            },
+            2: {
+                gridTemplateColumns: 'repeat(2, 1fr)',
+            },
+        },
+        center: {
+            true: {
+                justifyContent: 'center',
+            },
+        },
+        gap: {
+            1: {
+                gap: '$1',
+            },
+            2: {
+                gap: '$2',
+            },
+            3: {
+                gap: '$3',
+            },
+            4: {
+                gap: '$4',
+            },
+            5: {
+                gap: '$5',
+            },
+            6: {
+                gap: '$6',
+            },
+            7: {
+                gap: '$7',
+            },
+            8: {
+                gap: '$8',
+            },
+            9: {
+                gap: '$9',
+            },
+        },
+    },
+    defaultVariants: {
+        gap: 8,
+    },
 });
 
 export default Grid;
