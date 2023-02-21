d3f18efce577f9e478d09bd52b25d81246df6e44
Jimmy Bodden <jdboddenp@gmail.com>
2023-02-21 11:38:36 -0600

    Added wrapper container specific to header component

--- components/organisms/PageHeader.tsx
+++ components/organisms/PageHeader.tsx
@@ -1,16 +1,27 @@
 import Typography from '../atoms/Typography';
 import Stack from '../layouts/Stack';
+import { styled } from '@/stitches.config';
 
 export interface PageHeaderProps {
     title: string;
 }
 
+const HeaderContainer = styled(Stack, {
+    height: '92px',
+    backgroundColor: 'white',
+    boxShadow: '0px 0px 12px rgba(0, 0, 0, 0.07)',
+});
+
 const PageHeader = (props: PageHeaderProps) => {
     const { title } = props;
     return (
-        <Stack direction="row" alignItems="center">
+        <HeaderContainer
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+        >
             <Typography variant="pageHeader">{title}</Typography>
-        </Stack>
+        </HeaderContainer>
     );
 };
 
